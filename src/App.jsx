e0ff8298209113e87a647b0e6d799b236ffb92bc
beqa <beqa.tskhvediani@gmail.com>
2024-03-08 10:32:01 +0400

    add GlobalStyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import { GlobalStyles } from "./Global";
+import styled from "styled-components";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100..900&display=swap');
+body{
+ min-height:100vh;
+ background: linear-gradient(180deg, #B068E9 0%, #6463E7 100%);
+display: flex;
+justify-content: center;
+align-items: center;
+}
+`;
