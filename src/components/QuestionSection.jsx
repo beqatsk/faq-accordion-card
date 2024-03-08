5c640eb258acd626c2ae8229a5d6cc50e3e5fac4
beqa <beqa.tskhvediani@gmail.com>
2024-03-08 19:24:53 +0400

    add props

--- src/components/QuestionSection.jsx
+++ src/components/QuestionSection.jsx
@@ -5,9 +5,21 @@ export default function QuestionSection({ questionIndex, active, setActive }) {
   const question = data[questionIndex].question;
   const answer = data[questionIndex].answer;
   return (
-    <QuestionContainer onClick={() => setActive(questionIndex)}>
+    <QuestionContainer
+      onClick={() => {
+        if (active === questionIndex) {
+          setActive(null);
+        } else {
+          setActive(questionIndex);
+        }
+      }}
+      active={active}
+      questionIndex={questionIndex}
+    >
       <div>
-        <Question>{question}</Question>
+        <Question active={active} questionIndex={questionIndex}>
+          {question}
+        </Question>
         <img src={Arrow} alt="arrowimage" />
       </div>
       {active === questionIndex && <Answer>{answer}</Answer>}
@@ -27,13 +39,26 @@ const QuestionContainer = styled.section`
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    & > img {
+      transform: ${(props) =>
+        props.active === props.questionIndex
+          ? "rotate(180deg)"
+          : "rotate(0deg)"};
+      transition: 0.5s;
+    }
   }
 `;
 const Question = styled.h2`
   font-size: 1.3rem;
-  font-weight: 400;
+  font-weight: ${(props) =>
+    props.active === props.questionIndex ? "700" : "400"};
   line-height: 1.6rem;
   color: #1e1f36;
+
+  &:hover {
+    color: #f47b56;
+  }
 `;
 const Answer = styled.p`
   font-size: 1.2rem;
